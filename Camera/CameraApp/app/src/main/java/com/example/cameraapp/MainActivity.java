90b2ec42ea2ec68163e1dde62b2f87fb00b01a2c
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-21 23:20:34 +0000

    Made changes for saving image file (still has bugs)

--- Camera/CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
+++ Camera/CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.ImageView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -21,6 +20,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    String currentPhotoPath;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -32,10 +32,62 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView)findViewById(R.id.imageView);
 
         handlePhoto();
+
     }
 
-    protected void handlePhoto(){
+    // function that adds the photo that was taken to the device gallery
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.cameraapp",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    // function that launches camera from button press and starts listener
+
+    protected void handlePhoto(){
         Button takePhoto = (Button)findViewById(R.id.btnCamera);
 
         /* Reference - taken from https://alvinalexander.com/source-code/android/android-how-add-
@@ -43,14 +95,19 @@ public class MainActivity extends AppCompatActivity {
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+                dispatchTakePictureIntent();
+                galleryAddPic();
             }
         });
         /* End of Reference */
+
     }
 
+    // function that sets the app's main section to the thumbnail of the photo that was taken
+    // as a Bitmap
+
     /* Reference - taken from https://developer.android.com/training/camera/photobasics.html#java */
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
