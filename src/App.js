c1fd03d52d88400536421429f7daaaa0bce9344b
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-23 15:44:09 +0000

    Testing VS git

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Has this change been made</code> and save to reload.
+          <code>Has this change been made</code> and save to reload.
         </p>
         <a
           className="App-link"
