c22e5370c4df6a2b68d7d52d5e11a07deabfec27
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-23 17:00:10 +0000

    Added SQL call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import picture from './output_2.png';
 import './App.css';
+import createTable from './Helpers/database-helper';
 
 function App() {
+
+  createTable()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={picture} className="App-logo" alt="logo" />
-        <p>
-          <code>Has this change been made</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/Helpers/database-helper.js
@@ -0,0 +1,15 @@
+const { Client } = require('pg');
+export default function createTable(){
+    const client = new Client({
+        connectionString: "ec2-54-195-252-243.eu-west-1.compute.amazonaws.com",
+        ssl: true,
+      });
+      
+      client.connect();
+      
+      client.query('create table patients ( id SERIAL PRIMARY KEY,familyname varchar(128) NOT NULL,givenname varchar(128) NOT NULL,phonenumber varchar(32));', (err, res) => {
+        if (err) throw err;
+        client.end();
+      });
+}
+
