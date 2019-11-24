e80da480167a83ce7ca4551299607ccb205e9ab6
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 16:24:45 +0000

    Added some buttons

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,11 @@ class Search extends Component {
     render() { 
         return (
             <div class="md-form mt-0">
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+                <span>
+                    <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+                    <button className = "btn btn-success btn-m m-2"></button>
+                    <button className = "btn btn-success btn-m m-2"></button>
+                </span>
             </div>
         );
     }
