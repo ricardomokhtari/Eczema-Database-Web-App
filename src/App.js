a3a4c5c8956ca7a07e89a8349f8f473e44f22595
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 19:02:06 +0000

    Added more patients and changed styling

--- src/App.js
+++ src/App.js
@@ -37,12 +37,78 @@ class App extends Component {
           <button type="button" class="btn btn-primary">View</button>
           <button type="button" class="btn btn-primary">Upload</button>
         </div>
+      ],
+      [
+        '1234',
+        'M',
+        '01/01/1985',
+        '65',
+        '24/11/2019',
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-primary">View</button>
+          <button type="button" class="btn btn-primary">Upload</button>
+        </div>
+      ],
+      [
+        '1234',
+        'M',
+        '01/01/1985',
+        '65',
+        '24/11/2019',
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-primary">View</button>
+          <button type="button" class="btn btn-primary">Upload</button>
+        </div>
+      ],
+      [
+        '1234',
+        'M',
+        '01/01/1985',
+        '65',
+        '24/11/2019',
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-primary">View</button>
+          <button type="button" class="btn btn-primary">Upload</button>
+        </div>
+      ],
+      [
+        '1234',
+        'M',
+        '01/01/1985',
+        '65',
+        '24/11/2019',
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-primary">View</button>
+          <button type="button" class="btn btn-primary">Upload</button>
+        </div>
+      ],
+      [
+        '1234',
+        'M',
+        '01/01/1985',
+        '65',
+        '24/11/2019',
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-primary">View</button>
+          <button type="button" class="btn btn-primary">Upload</button>
+        </div>
+      ],
+      [
+        '1234',
+        'M',
+        '01/01/1985',
+        '65',
+        '24/11/2019',
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-primary">View</button>
+          <button type="button" class="btn btn-primary">Upload</button>
+        </div>
       ]
     ];
 
     return (
       <React.Fragment>
-        <div>
+        <div className = "TopBar">
           <Search/>
         </div>
         <div className = "App-header">
