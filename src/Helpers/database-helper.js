29bcad2044ab792b1b1e0a48e88698ace048af11
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 11:10:19 +0000

    Replaced helper with default code

--- src/Helpers/database-helper.js
+++ src/Helpers/database-helper.js
@@ -2,15 +2,18 @@ export default function createTable(){
     const { Client } = require('pg');
 
     const client = new Client({
-        connectionString: "postgres://hdzsfwizwsoxxz:b62525ba2575aa2a67faab4ab24f80234d850305522338e2bb16dd02f474db8e@ec2-54-195-252-243.eu-west-1.compute.amazonaws.com:5432/din8m6nuaj",
-        ssl: true,
-      });
-      
-      client.connect();
-      
-      client.query('create table patients ( id SERIAL PRIMARY KEY, familyname varchar(128) NOT NULL, givenname varchar(128) NOT NULL, phonenumber varchar(32));', (err, res) => {
-        if (err) throw err;
-        client.end();
-      });
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+    });
+
+    client.connect();
+
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+    });
 }
 
