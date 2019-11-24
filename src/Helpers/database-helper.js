c13ab3a744d551fb0483a75ee56e5c19a603031e
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 11:07:19 +0000

    Updated connection string

--- src/Helpers/database-helper.js
+++ src/Helpers/database-helper.js
@@ -2,7 +2,7 @@ export default function createTable(){
     const { Client } = require('pg');
 
     const client = new Client({
-        connectionString: process.env.DATABASE_URL,
+        connectionString: "postgres://hdzsfwizwsoxxz:b62525ba2575aa2a67faab4ab24f80234d850305522338e2bb16dd02f474db8e@ec2-54-195-252-243.eu-west-1.compute.amazonaws.com:5432/din8m6nuaj",
         ssl: true,
       });
       
