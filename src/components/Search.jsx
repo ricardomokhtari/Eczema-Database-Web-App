b6b8e4fd98a2916a55a6ae8d192953b35edaf383
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 17:27:03 +0000

    Changed styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,9 +4,7 @@ class Search extends Component {
     render() { 
         return (
             <div className="input-group">
-                <div className="active-cyan-3 active-cyan-4">
-                    <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
-                </div>
+                <input class="form-control" type="text" placeholder="Search patients" aria-label="Search"></input>
                 <span className="input-group-btn">
                     <button className="btn btn-success">Add New Patient</button>
                 </span>
