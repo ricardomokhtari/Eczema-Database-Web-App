f223dde64e42c852a3b543ce40e6a252135988a0
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 16:34:05 +0000

    Testing inline

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,11 +4,8 @@ class Search extends Component {
     render() { 
         return (
             <div class="md-form mt-0">
-                <span>
-                    <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
-                    <button className = "btn btn-success btn-m m-2"></button>
-                    <button className = "btn btn-success btn-m m-2"></button>
-                </span>
+                <span><input class="form-control" type="text" placeholder="Search" aria-label="Search"></input></span>
+                <button className = "btn btn-success btn-m m-2">Add New Patient</button>
             </div>
         );
     }
