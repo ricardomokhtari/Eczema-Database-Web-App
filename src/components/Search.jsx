bf96e36311c9e1ca433613cb9965472358053364
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 17:18:56 +0000

    Search bar and button now inline

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class Search extends Component {
     render() { 
         return (
-            <div>
-                <span>
-                    <input class="form-control" type="text" width="50px" placeholder="Search" aria-label="Search"></input>
-                    <button className = "btn btn-success btn-m m-2">Add New Patient</button>
+            <div className="input-group">
+                <div className="active-cyan-3 active-cyan-4">
+                    <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+                </div>
+                <span className="input-group-btn">
+                    <button className="btn btn-success">Add New Patient</button>
                 </span>
             </div>
         );
