05d41825a65829c40f6db464ff8ecdc480718840
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 10:37:17 +0000

    Wrapped in function

--- src/Helpers/database-helper.js
+++ src/Helpers/database-helper.js
@@ -1,16 +1,19 @@
 const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+export default function createTable(){
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+      });
+      
+      client.connect();
+      
+      client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+          console.log(JSON.stringify(row));
+        }
+        client.end();
+      });
+}
 
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
