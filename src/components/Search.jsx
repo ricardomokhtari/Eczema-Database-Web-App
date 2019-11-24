0ee1e0b39a1e1ef420af735a8dd24897d954b01e
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-24 16:19:15 +0000

    Added search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render() { 
+        return (
+            <div class="md-form mt-0">
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+            </div>
+        );
+    }
+}
+ 
+export default Search;
