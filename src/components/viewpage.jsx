a89ffb820d9a82f31d7143f0111a5d96a0e8f765
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-27 16:52:57 +0000

    Added a basic side bar

--- src/components/viewpage.jsx
+++ src/components/viewpage.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import './SideNav.css';
 
 function ViewPage() {
     return (
         <React.Fragment>
-            <div>
-                <p>Fuck yes</p>
-            </div>
+            <SideNav
+                onSelect={(selected) => {
+                    // Add your code here
+                }}
+            >
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="home">
+                    <NavItem eventKey="home">
+                        <NavIcon>
+                            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                        </NavIcon>
+                        <NavText>
+                            Records
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="charts">
+                        <NavIcon>
+                            <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                        </NavIcon>
+                        <NavText>
+                            Upload
+                        </NavText>
+                    </NavItem>
+                </SideNav.Nav>
+            </SideNav>
+
         </React.Fragment>
+
     );
 }
  
