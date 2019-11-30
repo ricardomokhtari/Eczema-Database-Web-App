e326d3827ff33e5471c597c17793221387031c19
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-30 15:25:11 +0000

    Made text on right of sidebar responsive

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -3,34 +3,53 @@ import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/rea
 import './SideNav.css';
 
 class UploadPage extends Component {
+    state = {
+        expanded: false
+    };
+
+    onToggle = (expanded) => {
+        this.setState({ expanded: expanded });
+    };
+    
     render() { 
+        const {expanded} = this.state;
         return (
             <React.Fragment>
-            <SideNav
-                onSelect={(selected) => {
-                    // Add your code here
-                }}
-            >
-                <SideNav.Toggle />
-                <SideNav.Nav defaultSelected="charts">
-                    <NavItem eventKey="charts">
-                        <NavIcon>
-                            <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
-                        <NavText>
-                            Upload
-                        </NavText>
-                    </NavItem>
-                    <NavItem eventKey="home">
-                        <NavIcon>
-                            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
-                        <NavText>
-                            Records
-                        </NavText>
-                    </NavItem>
-                </SideNav.Nav>
-            </SideNav>
+                <div 
+                 style={{
+                    marginLeft: expanded ? 240 : 64,
+                    padding: '15px 20px 0 20px'
+                }}>
+                    <p>Hello World</p>
+
+
+
+                    
+                <div>
+                    <SideNav onToggle = {this.onToggle}>
+                        <SideNav.Toggle />
+                        <SideNav.Nav defaultSelected="charts">
+                            <NavItem eventKey="charts">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText>
+                                    Upload
+                                </NavText>
+                            </NavItem>
+                            <NavItem eventKey="home">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText>
+                                    Records
+                                </NavText>
+                            </NavItem>
+                        </SideNav.Nav>
+                    </SideNav>
+                </div>
+                </div>
+                
         </React.Fragment>
         );
     }
