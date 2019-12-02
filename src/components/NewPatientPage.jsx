e19f826f2d6caff125937b61914948620190d527
HM00 <hiaming01@hotmail.com>
2019-12-02 16:06:28 +0000

    Added left margin to new patient page

--- src/components/NewPatientPage.jsx
+++ src/components/NewPatientPage.jsx
@@ -5,32 +5,38 @@ class NewPatientPage extends Component {
     render() {
         return (
             <React.Fragment>
-                    <div>
-                        <h1>New Patient</h1>
-                    </div>
-                    <div className = "Inline">
-                        <h5>First Name</h5>
-                        <input className = "Input" type="text" placeholder = "John"/>
-                    </div>
-                    <div className = "Inline">
-                        <h5>Last Name</h5>
-                        <input className = "Input" type="text" placeholder = "Smith"/>
-                    </div>
-                    <div className = "Inline">
-                        <h5>Sex</h5>
-                        <input className = "Input" type="text" placeholder = "M"/>
-                    </div>
-                    <div className = "Inline">
-                        <h5>Ethnicity</h5>
-                        <input className = "Input" type="text" placeholder = "Caucasian"/>
-                    </div>
-                    <div className = "Inline">
-                        <h5>Date of Birth</h5>
-                        <input className = "Input" type="text" placeholder = "1/1/1990"/>
-                    </div>
-                    <div>
-                        <button className = "btn btn-info m-2">Add Patient</button>
-                    </div>
+              <div
+               style={{
+                  marginLeft: expanded ? 240 : 64,
+                  padding: '15px 20px 0 20px'
+              }}>
+                <div>
+                    <h1>New Patient</h1>
+                </div>
+                <div className = "Inline">
+                    <h5>First Name</h5>
+                    <input className = "Input" type="text" placeholder = "John"/>
+                </div>
+                <div className = "Inline">
+                    <h5>Last Name</h5>
+                    <input className = "Input" type="text" placeholder = "Smith"/>
+                </div>
+                <div className = "Inline">
+                    <h5>Sex</h5>
+                    <input className = "Input" type="text" placeholder = "M"/>
+                </div>
+                <div className = "Inline">
+                    <h5>Ethnicity</h5>
+                    <input className = "Input" type="text" placeholder = "Caucasian"/>
+                </div>
+                <div className = "Inline">
+                    <h5>Date of Birth</h5>
+                    <input className = "Input" type="text" placeholder = "1/1/1990"/>
+                </div>
+                <div>
+                    <button className = "btn btn-info m-2">Add Patient</button>
+                </div>
+              </div>
             </React.Fragment>
         );
     }
