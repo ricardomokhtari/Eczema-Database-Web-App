1cbc678ac4d625eae7a079ac4c96ffcfd5f254cd
ricardomokhtari <ricardomokhtari@gmail.com>
2019-12-02 15:21:19 +0000

    Added table to upload page

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import './SideNav.css';
 import './UploadPage.css';
+import DataTable from './DataTable';
+import './DataTable.css';
 import {
     BrowserRouter as Router,
     Switch,
@@ -17,9 +19,40 @@ class UploadPage extends Component {
     onToggle = (expanded) => {
         this.setState({ expanded: expanded });
     };
+
+    
     
     render() { 
         const {expanded} = this.state;
+
+        const headings = [
+            'Erythema',
+            'Edema',
+            'Excoriation',
+            'Lichenification',
+            'Area Score',
+        ];
+      
+        const rows = [
+            [
+                <div>
+                    <input maxLength = "1" className = "TableInput" type="text"></input>
+                </div>,
+                <div>
+                    <input maxLength = "1" className = "TableInput" type="text"></input>
+                </div>,
+                <div>
+                    <input  maxLength = "1" className = "TableInput" type="text"></input>
+                </div>,
+                <div>
+                    <input maxLength = "1" className = "TableInput" type="text"></input>
+                </div>,
+                <div>
+                    <input maxLength = "1" className = "TableInput" type="text"></input>
+                </div>
+            ]
+        ];
+
         return (
             <React.Fragment>
                 <div 
@@ -38,11 +71,14 @@ class UploadPage extends Component {
                         <h5>Region</h5>
                         <input className = "Input" type="text" placeholder = "eg. Head"/>
                     </div>
+                    <div>
+                        <DataTable headings = {headings} rows = {rows}></DataTable>
+                    </div>
                     <div className = "Inline">
                         <h5>Score:</h5>
                     </div>
                     <div>
-                        <button className = "btn btn-info m-2">Upload</button>
+                        <input type="button" value="Choose Files!" onclick="document.getElementById('fileInput').click();" />                    
                     </div>
                 </div>
 
