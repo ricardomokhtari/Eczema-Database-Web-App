27b380268ab8bd230629925c2dca1377890d07c2
HM00 <hiaming01@hotmail.com>
2019-12-02 15:45:41 +0000

    Fixed minor bug

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class Search extends Component {
     render() {
