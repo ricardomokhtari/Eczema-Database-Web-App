b8a58217410e52933f5303cfe907a46f51b68b46
HM00 <hiaming01@hotmail.com>
2019-12-04 11:37:15 +0000

    setFields

--- src/components/NewPatientPage.jsx
+++ src/components/NewPatientPage.jsx
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import './NewPatientPage.css';
 
 class NewPatientPage extends Component {
+
+
+    constructor(props){
+      super(props);
+      this.state = {
+        firstame: null,
+        lastname: null,
+        sex: null,
+        ethinicity: null,
+        dateofbirth: null
+      };
+      this.setFields = this.setFields.bind(this);
+    }
+
+    setFields(e){
+      this.setState({
+      firstname: this.refs.firstname.value,
+      lastname: this.refs.lastname.value,
+      sex: this.refs.sex.value,
+      ethnicity: this.refs.ethnicity.value,
+      dateofbirth: this.refs.dateofbirth.value
+      });
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -15,27 +39,30 @@ class NewPatientPage extends Component {
                 </div>
                 <div className = "Inline">
                     <h5>First Name</h5>
-                    <input className = "Input" type="text" placeholder = "John"/>
+                    <input className = "Input" type="text" ref="firstname" placeholder = "John"/>
                 </div>
                 <div className = "Inline">
                     <h5>Last Name</h5>
-                    <input className = "Input" type="text" placeholder = "Smith"/>
+                    <input className = "Input" type="text" ref="lastname" placeholder = "Smith"/>
                 </div>
                 <div className = "Inline">
                     <h5>Sex</h5>
-                    <input className = "Input" type="text" placeholder = "M"/>
+                    <input className = "Input" type="text" ref="sex" placeholder = "M"/>
                 </div>
                 <div className = "Inline">
                     <h5>Ethnicity</h5>
-                    <input className = "Input" type="text" placeholder = "Caucasian"/>
+                    <input className = "Input" type="text" ref="ethnicity" placeholder = "Caucasian"/>
                 </div>
                 <div className = "Inline">
                     <h5>Date of Birth</h5>
-                    <input className = "Input" type="text" placeholder = "1/1/1990"/>
+                    <input className = "Input" type="text" ref="dateofbirth" placeholder = "1/1/1990"/>
                 </div>
                 <div>
-                    <button className = "btn btn-info m-2">Add Patient</button>
+                    <button className = "btn btn-info m-2" onClick={this.setFields}>Add Patient</button>
                 </div>
+              </div>
+              <div>
+                {this.state.firstname}
               </div>
             </React.Fragment>
         );
