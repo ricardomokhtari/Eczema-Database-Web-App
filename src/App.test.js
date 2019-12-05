465f55741903cfe94781be9323a3c202599e6fdc
ricardomokhtari <ricardomokhtari@gmail.com>
2019-12-05 15:18:33 +0000

    Added more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Homepage from './components/Homepage';
+import UploadPage from './components/UploadPage';
+import NewPatientPage from './components/NewPatientPage';
+import ViewPage from './components/viewpage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Homepage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<UploadPage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<NewPatientPage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ViewPage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
